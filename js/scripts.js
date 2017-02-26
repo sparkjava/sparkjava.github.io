75b2da8beef2f5d1efe6c0c69be37557bd154967
David <davidaase@hotmail.com>
2017-02-27 00:19:35 +0100

    Put javascript in _include after all apparently ...

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 ---
 ---
 
-{% include_relative _partials/vendor/prism.min.js %}
-{% include_relative _partials/vendor/smoothScroll.min.js %}
-{% include_relative _partials/main.js %}
+{% include javascript/vendor/prism.min.js %}
+{% include javascript/vendor/smoothScroll.min.js %}
+{% include javascript/main.js %}
