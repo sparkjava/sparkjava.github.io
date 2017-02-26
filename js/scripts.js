411fbe67243352eca4a89dece41180a7c8237476
David <davidaase@hotmail.com>
2017-02-27 00:21:35 +0100

    So sensitive...

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,5 @@
 ---
 
 {% include_relative _partials/vendor/prism.min.js %}
-{% include_relative _partials/vendor/smoothScroll.min.js %}
+{% include_relative _partials/vendor/smoothscroll.min.js %}
 {% include_relative _partials/main.js %}
